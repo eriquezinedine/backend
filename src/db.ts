ab4c7da77006ce89d8461e0eef50f4b48e5dbd45
eriquezinedine <eriquezinedine@gmail.com>
2023-01-12 18:10:46 -0500

    fast

--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,7 @@ import { User } from './entities/User';
 import { Influencer } from './entities/Influencer';
 // import { Photo } from './entities/Photo';
 import { Services } from './entities/Services';
+import { DB_PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } from './config';
 
 // export const AppDataSource = new DataSource({
 //     type: "postgres",
@@ -18,11 +19,11 @@ import { Services } from './entities/Services';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "123456789",
-    database: "huachowoman",
+    host: DB_HOST,
+    port: parseInt(DB_PORT + ""),
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
     entities: [ User, Influencer,Services ],
     logging: true,
     synchronize: true, //! Puedo observar todo el codigo que se genera
